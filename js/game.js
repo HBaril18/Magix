8ac86ae12b4dffdd0915a59ec19a15c74b4ec41f
HBaril18 <barilhenrick@gmail.com>
2023-11-08 23:31:48 -0500

    essaie d'un vérificateur de main de carte

--- js/game.js
+++ js/game.js
@@ -27,20 +27,20 @@ const state = () => {
             document.querySelector("#mp").innerText = data.mp;
 
             //GESTION DE LA MAIN DU JOUEUR
-            if(!isset($_SESSION["mainNbr"])){ //isset n'existe pas en js trouver équivalent
-                $_SESSION["mainNbr"] = 0;
-                console.log($_SESSION["mainNbr"]);
-            }
-            if ($_SESSION["mainNbr"] < maVariable["hand"].length) {
-                console.log("hey");
-                $_SESSION["mainNbr"] = maVariable["hand"].length;
+            //if(!$_SESSION["mainNbr"]){ //isset n'existe pas en js trouver équivalent
+                //$_SESSION["mainNbr"] = 0;
+                //console.log($_SESSION["mainNbr"]);
+           // }
+            //if ($_SESSION["mainNbr"] < maVariable["hand"].length) {
+               // console.log("hey");
+                //$_SESSION["mainNbr"] = maVariable["hand"].length;
                 document.querySelector("#main").innerHTML = "";
                 for (let index = 0; index < maVariable["hand"].length; index++) {
                     let newNode = document.createElement("div");
                     newNode.innerText = maVariable["hand"][index].id;
                     document.querySelector("#main").append(newNode);
                 }
-            }
+            //}
             
             //GESTION DE LA MAIN DE L'OPPOSANT
             document.querySelector("#main-opponent").innerHTML = "";
